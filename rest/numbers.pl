446dbe5d26768a95d7f504534cc2f4a44bebfcd2
Roman Galeev <jamhed@2600hz.com>
2016-09-19 14:42:20 +0200

    move rest works to separate project

--- rest/numbers.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use KZ;
-
-$KZ::verbose = 1;
-my $auth = login 'admin', 'admin', 'admin';
-
-add_spare_number token $auth, account_id $auth->{data}, "+14157775553";
